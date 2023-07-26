b19885019f5060b757e0e66fb1b382549395983f
NaoomAouf <s665183@ese.gov.ae>
2023-07-26 19:32:10 +0400

    Changed the text and add my name .

--- github demo upload/ContentView.swift
+++ github demo upload/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Naoom !")
             Text("I am genius")
             Text("Sooo excited !")
         }
